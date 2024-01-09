ec175dd9b62587d2e1649619d1d0d6e3708bbc9a
Jothish JJ <jothishjj11@gmail.com>
2024-01-09 10:59:18 +0530

    Initial commit

---
+++ mecha.js
@@ -0,0 +1,17 @@
+/**
+ * Function for mecha
+ *
+ * @returns {string} Big mecha
+ */
+export function mecha() {
+    return "Big mecha";
+}
+
+/**
+ * Function for Mechanic
+ *
+ * @returns {string} Mechanic
+ */
+export function Mechanic() {
+    return mecha() + "nic";
+}
